15ac9b909b8672b706c34f08d89a641a1f716c07
h3rbass <gamerstyle256@gmail.com>
2026-09-21 09:16:19 -0400

    Conexion a Supabase

--- Program.cs
+++ Program.cs
@@ -1,11 +1,36 @@
+using DSSeptiembre;
+using DSSeptiembre.Shared.Configuration;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using DSSeptiembre;
+using Supabase;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//conexion a supabase
+
+var supabaseOptions = builder.Configuration
+    .GetSection("Supabase")
+    .Get<SupabaseConfiguration>();
+
+var client = new Client(
+    supabaseOptions!.Url,
+    supabaseOptions.Key
+);
+
+await client.InitializeAsync();
+
+builder.Services.AddSingleton(client);
+Console.WriteLine(client);
+
+//fin conexion a supabase
+
+builder.Services.AddScoped(sp =>
+    new HttpClient
+    {
+        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+    });
 
 await builder.Build().RunAsync();
